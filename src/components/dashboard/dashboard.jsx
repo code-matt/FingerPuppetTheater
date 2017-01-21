32d49ddac2de094e6d6ee820522fb1eac591fa74
Matt T <code.matthew.thompson@gmail.com>
2017-01-21 13:10:01 -0500

    finger puppet

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import "./dashboard.css"
-import VideoComponent from "./video/video"
+import './dashboard.css'
+import VideoComponent from './video/video'
 
 export default class DashboardComponent extends React.Component {
   render () {
     return (
-      <div className="container">
-        <div className="row box1">
-          <div className="col-md-8 offset-md-2 box2">
-          <VideoComponent/>
+      <div className='container'>
+        <div className='row box1'>
+          <div className='col-md-8 offset-md-2 box2'>
+            <VideoComponent />
           </div>
-          <div className="col-md-8 offset-md-2 box3">
+          <div className='col-md-8 offset-md-2 box3'>
           ima dashboard
           </div>
         </div>
